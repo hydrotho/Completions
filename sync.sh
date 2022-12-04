57d668a39e5e22cfe498dbb06eb8e83a55221603
hydrotho <42911474+hydrotho@users.noreply.github.com>
2022-12-04 11:55:58 +0800

    feat(zoxide): add `_zoxide` completion for zsh

--- sync.sh
+++ sync.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 trap "echo -e '\e[1;31mScript failed: see failed command above\e[0m'" ERR
 
 wget --no-verbose -O _fzf https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh
+wget --no-verbose -O _zoxide https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/contrib/completions/_zoxide
